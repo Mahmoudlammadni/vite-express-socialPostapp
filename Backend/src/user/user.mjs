98944027012e656cdab1dea4c2063152a778cb41
Mahmoud <lamm.mah2002@gmail.com>
2025-01-24 23:45:47 +0100

    Add API route to add a new user with validation and hashed password

--- Backend/src/user/user.mjs
+++ Backend/src/user/user.mjs
@@ -17,6 +17,23 @@ user.get('/api/users', async(req,res)=>{
     const data = await user_coll.find()
     data?res.status(200).send(data):res.sendStatus(404)
 })
-
+user.post('/api/user/add',async(req,res)=>{
+    const {nom,email,password}=req.body
+    const data = await user_coll.find()
+    if (isNaN(nom && email )&& password) {
+   
+        const new_id = await data[data.length-1].id+1
+        const new_password= await bcrypt.hash(password,code)
+        const ob= {id:new_id,nom:nom,email:email,password:new_password}
+        const add = await user_coll.insertMany(ob)
+        const data2 = await user_coll.find()
+        if (add) {
+            res.status(200).send(data2)
+        }
+    }
+    else{
+        res.sendStatus(404)
+    }
+})
 
 export default user
