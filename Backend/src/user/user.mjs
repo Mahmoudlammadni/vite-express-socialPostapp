e35fc310d36eb4ccab6f2b88ccd539779d5ca16a
Mahmoud <lamm.mah2002@gmail.com>
2025-01-25 21:14:28 +0100

    adding the route of deleting a user

--- Backend/src/user/user.mjs
+++ Backend/src/user/user.mjs
@@ -17,11 +17,11 @@ user.get('/api/users', async(req,res)=>{
     const data = await user_coll.find()
     data?res.status(200).send(data):res.sendStatus(404)
 })
+
 user.post('/api/user/add',async(req,res)=>{
     const {nom,email,password}=req.body
     const data = await user_coll.find()
     if (isNaN(nom && email )&& password) {
-   
         const new_id = await data[data.length-1].id+1
         const new_password= await bcrypt.hash(password,code)
         const ob= {id:new_id,nom:nom,email:email,password:new_password}
@@ -36,4 +36,15 @@ user.post('/api/user/add',async(req,res)=>{
     }
 })
 
+user.delete('/api/user/delete/:id',async(req,res)=>{
+    const {params:{id}}=req
+    const parsed= parseInt(id)
+    if (!isNaN(parsed)) {
+        const del = await user_coll.findOneAndDelete({id:parsed})
+        del?res.status(200).send("Deleted successfully"):res.sendStatus(404)
+    }
+    else{
+        return res.sendStatus(400)
+    }
+})
 export default user
