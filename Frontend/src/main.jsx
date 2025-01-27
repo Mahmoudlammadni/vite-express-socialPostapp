5aefdf5084689379eb344c0f9300ad214f2f31cc
Mahmoud <lamm.mah2002@gmail.com>
2025-01-27 12:13:45 +0100

    add the form of loging with the route of authentification from backend

--- Frontend/src/main.jsx
+++ Frontend/src/main.jsx
@@ -1,7 +1,7 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 
-import App from './App.jsx'
+import App from './LogIn.jsx'
 import { Provider } from 'react-redux'
 import { legacy_createStore } from 'redux'
 import redux from './redux.jsx'
